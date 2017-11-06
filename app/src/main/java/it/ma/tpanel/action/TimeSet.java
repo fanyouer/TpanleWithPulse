38015c2c832a9f3a7f96419bf55650442b00c210
Krguang <526110805@qq.com>
2017-11-06 16:15:35 +0800

    修改了获取24小时为12小时制的bug

--- app/src/main/java/it/ma/tpanel/action/TimeSet.java
+++ app/src/main/java/it/ma/tpanel/action/TimeSet.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
-import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.TimePicker;
 import android.widget.Toast;
@@ -20,8 +19,7 @@ public class TimeSet extends Activity{
 
     private DatePicker dpToday;
     private TimePicker tpNow;
-    private Button timeSave;
-    private Button timeExit;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
